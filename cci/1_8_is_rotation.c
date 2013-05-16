27b4b9caaf73379bc3daa90401fee5792eef9885
goran333 <goran333@gmail.com>
2013-05-15 19:33:21 -0700

    renames

---
+++ cci/1_8_is_rotation.c
@@ -0,0 +1,12 @@
+bool isSubstring(const char *parent, const char *substring)
+{
+}
+
+bool isRotation(const char *s1, const char *s2)
+{
+  //if length is not same, return false
+  
+  //sort and compare strings, they should be equal, if not return false
+
+  //if s2 is a rotation of s1, then s2 will always be a substring of "s1s1"
+}
